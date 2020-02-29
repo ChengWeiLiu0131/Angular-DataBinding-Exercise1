26e2ff180caf54c1f184494ae30bd56d1389bef6
liu <lecx52579@gmail.com>
2020-03-01 02:35:47 +0800

    first commit with git bash

---
+++ src/app/app.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'app';
+  url = 'http://blog.miniasp.com/';
+  imgurl = '/assets/images/logo.png';
+  strLen = 0;
+  constructor() {
+  }
+  changeTitle(altKey: boolean) {
+    if (altKey) {
+      this.title  = 'The Will Will Web';
+    }
+  }
+
+  keyPressAction($event) {
+    console.log($event);
+    if( $event.key === 'Escape') {
+      $event.target.value = '';
+      this.strLen = 0;
+    }
+    this.strLen = $event.target.value.length;
+  }
+}
